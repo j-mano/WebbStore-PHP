d347a28ede7fcb51c6a8d11fb0c9472e226e1724
joachim <joachim@johnsons.nu>
2021-04-17 22:57:48 +0200

    Intial start of html file
    - Html files and folder structure added.
    - css files added
    - An simple meny created from other project(currently not relevant for this project.)

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>WebbShop</title>
+        <meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline';" />
+        
+        <!-- Scripts -->
+        <script></script>
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="css/Style.css">
+        <link rel="stylesheet" href="css/StyleMobile" media="handheld">
+    </head>
+    <body>
+        <?php
+            readfile("Header.php");
+        ?>
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+        <?php
+            readfile("Foter.php");
+        ?>
+    </body>
+</html>
